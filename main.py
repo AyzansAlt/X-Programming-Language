5eca0473a109c55fd473114c6559c8acec259f16
AyzansAlt <daberdabs24@gmail.com>
2020-06-26 07:20:57 +0000

    RookOut

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+
+import rook
+
+ 
+
+if __name__ == "__main__":
+
+    rook.start(token='d05c5d380f0ac95c0ebe4b38baae5a2820869a9420eb9c253bbcdaf39a3f54f6')
 import setup as s
 import commands as cmds
 xp = "X+"
+
+#As a variable so i can use it in other fikes
 
 pointer = f"{s.bcolors.OKGREEN}>>> {s.bcolors.ENDC}"
 
